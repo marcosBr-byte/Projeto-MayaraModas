e8c46a9c90b0baa74eb1c797a1ebfaf9fe472835
root <root@marcos.localdomain>
2026-08-14 20:37:35 -0300

    atualizado o model carrinho

--- app/models/carrinho.rb
+++ app/models/carrinho.rb
@@ -1,3 +1,6 @@
 class Carrinho < ApplicationRecord
   belongs_to :user
+  has_many :itemcarrinhos, dependent: :destroy
+  has_many :produtos, through: :itemcarrinhos
+  validates :total, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 end
