96817c35a1f45b79fb1f4c1c87540c5ab94ee0c5
root <root@marcos.localdomain>
2026-08-14 20:02:59 -0300

    modificado o home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @destaques = [
+    Produto.find_by(categoria: "feminino"),
+    Produto.find_by(categoria: "masculino"),
+    Produto.find_by(categoria: "kids")
+  ].compact
   end
 end
