928df301d8ab8c2470f74abf9d3abb7a75528d50
root <root@marcos.localdomain>
2026-08-14 20:36:44 -0300

    atualizado o model produto

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,7 @@
 class Produto < ApplicationRecord
+  validates :nome, presence: true
+  validates :preco, presence: true, numericality: { greater_than: 0 }
+  validates :categoria, presence: true, inclusion: { in: %w[masculino feminino kids] }
+  has_many :itemcarrinhos
+  has_many :carrinhos, through: :itemcarrinhos
 end
