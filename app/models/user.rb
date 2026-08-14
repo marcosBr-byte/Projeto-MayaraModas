1f8916bc33d3d164ecae3cbc2c47085310bce7d1
root <root@marcos.localdomain>
2026-08-14 20:35:37 -0300

    atualizado o model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :nome, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  has_one :carrinho, dependent: :destroy
 end
