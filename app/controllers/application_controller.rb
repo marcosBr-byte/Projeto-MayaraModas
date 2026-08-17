f5c8a5745cbd4ec4d94ed37d3ead2e2718a43c09
root <root@marcos.localdomain>
2026-08-17 13:15:07 -0300

    adicionado a respeito do usuário

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,30 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  helper_method :usuario_atual, :logado?
+
+  private
+
+  def usuario_atual
+    @usuario_atual ||= User.find_by(id: session[:user_id])
+  end
+
+  def logado?
+    usuario_atual.present?
+  end
+
+  def requer_login
+    unless logado?
+      flash[:alert] = "Você precisa estar logado para acessar essa página."
+      redirect_to "/user/login"
+    end
+  end
+
+  def requer_admin
+    unless usuario_atual&.admin?
+      flash[:alert] = "Acesso restrito a administradores."
+      redirect_to root_path
+    end
+  end
 end
