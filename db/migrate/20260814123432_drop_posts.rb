a08874f798a6db76d9387f978cddf5f8e719933e
root <root@marcos.localdomain>
2026-08-21 10:11:24 -0300

    excluido o db migrate posts

--- db/migrate/20260814123432_drop_posts.rb
+++
@@ -1,9 +0,0 @@
-class DropPosts < ActiveRecord::Migration[8.1]
-  def change
-    drop_table :posts do |t|
-      t.string "title"
-      t.text "description"
-      t.timestamps
-    end
-  end
-end
